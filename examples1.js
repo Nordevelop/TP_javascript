ce08446d2c57ce01676adb211ecc58520cfaef59
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-01-07 10:56:04 -0300

    Create examples1.js

---
+++ examples1.js
@@ -0,0 +1,11 @@
+var n1 = "welcome to course javascript"; //ubicaciones de los elementos[] del array
+var enesima_letra;
+var ultima_letra;
+var tercera_letra =n1[2];
+const cuartaletra = n1[3];
+enesima_letra = n1[n1.length-6];
+console.log(n1); //imprime en pantalla 
+console.log("welcome to course javascript".length); //para saber la catidad de elementos del array
+console.log(enesima_letra);
+
+
