7f0247ec443e59ddf89c26114ece48b8f46c7fa8
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-01-07 11:04:23 -0300

    Update examples1.js

--- examples1.js
+++ examples1.js
@@ -2,10 +2,16 @@ var n1 = "welcome to course javascript"; //ubicaciones de los elementos[] del ar
 var enesima_letra;
 var ultima_letra;
 var tercera_letra =n1[2];
-const cuartaletra = n1[3];
+var cuarta_letra = n1[3];
 enesima_letra = n1[n1.length-6];
+penultima_letra =n1[n1.length-2];
+ultima_letra = n1[n1.length-1]
 console.log(n1); //imprime en pantalla 
 console.log("welcome to course javascript".length); //para saber la catidad de elementos del array
+console.log(tercera_letra)
+console.log(cuarta_letra)
 console.log(enesima_letra);
+console.log(penultima_letra);
+console.log(ultima_letra);
 
 
